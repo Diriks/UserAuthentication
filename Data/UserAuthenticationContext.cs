7347929cb189dd8ae1f16b6434d61964364c6910
Chrysa <Chrysa@DESKTOP-19K4ELI>
2023-08-16 00:47:25 -0400

    Add project files.

---
+++ Data/UserAuthenticationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserAuthentication.Models;
+
+namespace UserAuthentication.Data
+{
+    public class UserAuthenticationContext : IdentityDbContext
+    {
+        public UserAuthenticationContext (DbContextOptions<UserAuthenticationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserAuthentication.Models.UserModel> UserModel { get; set; } = default!;
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAuthentication.Models
+{
+    public class UserModel
+    {
+        public string Id { set; get; }
+        [Required(ErrorMessage = "Please enter Username")]
+        public string Username { set; get; }
+
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [Required(ErrorMessage = "Please enter Email")]
+        public string Email { set; get; }
+
+        [Required(ErrorMessage = "Please enter Password")]
+        public string Password { set; get; }
+
+        [Compare("Password", ErrorMessage = "Password does not match")]
+        [Required(ErrorMessage = "Please re-enter Password")]
+        public string ConfirmPassword { set; get; }
+
+        [RegularExpression("^\\d{9}$", ErrorMessage = "Please enter a correct phone number")]
+        public string Contact { set; get; }
+
+
+    }
+}
